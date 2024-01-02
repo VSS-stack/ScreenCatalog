4b832af013262f35eb4884de017e368dc1f735b3
VSS-stack <victor.stsilva93@gmail.com>
2024-01-02 09:53:33 -0300

    Adding Spring configurations

---
+++ src/main/java/com/example/screencatalog/ScreencatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.screencatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreencatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreencatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/screencatalog/ScreencatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.screencatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreencatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
