18b1b0a69e75451deed3a3ebef6f8d4743d2c321
VSS-stack <victor.stsilva93@gmail.com>
2024-01-04 13:38:15 -0300

    Creating a second class type to be converted from a json and adding comments

--- src/main/java/com/example/screencatalog/ScreencatalogApplication.java
+++ src/main/java/com/example/screencatalog/ScreencatalogApplication.java
@@ -1,5 +1,6 @@
 package com.example.screencatalog;
 
+import com.example.screencatalog.model.EpisodeData;
 import com.example.screencatalog.model.SerieData;
 import com.example.screencatalog.service.ApiConsumption;
 import com.example.screencatalog.service.ConvertData;
@@ -16,12 +17,23 @@ public class ScreencatalogApplication implements CommandLineRunner { //Interface
 
 	@Override
 	public void run(String... args) throws Exception { //Implements run method
+		/* Create API consumption object */
 		var apiConsumption = new ApiConsumption();
+
+		/* Make a request and return a string */
 		var json = apiConsumption.getData("http://www.omdbapi.com/?t=gilmore+girls&apikey=306f71f9");
 		System.out.println(json);
 
+		/* Create converter string to object */
 		ConvertData converter = new ConvertData();
-		SerieData newData = converter.getData(json, SerieData.class);
-		System.out.println(newData);
+
+		/* Convert a string into a SerieData object */
+		SerieData newSerie = converter.getData(json, SerieData.class);
+		System.out.println(newSerie);
+
+		/* Convert a string into a EpisodeData object */
+		json = apiConsumption.getData("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=306f71f9");
+		EpisodeData newEpisode = converter.getData(json, EpisodeData.class);
+		System.out.println(newEpisode);
 	}
 }
---
+++ src/main/java/com/example/screencatalog/model/EpisodeData.java
@@ -0,0 +1,11 @@
+package com.example.screencatalog.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record EpisodeData(@JsonAlias("Title") String title,
+                          @JsonAlias("Episode") Integer episodeNumber,
+                          @JsonAlias("imdbRating") String rating,
+                          @JsonAlias("Released") String releasingDate) {
+}
