d0183ba7464a35b8a08b8001ced5d61b405bbf45
VSS-stack <victor.stsilva93@gmail.com>
2024-01-06 17:36:02 -0300

    Transfering some code from the run method to the principal class

--- src/main/java/com/example/screencatalog/ScreencatalogApplication.java
+++ src/main/java/com/example/screencatalog/ScreencatalogApplication.java
@@ -1,16 +1,9 @@
 package com.example.screencatalog;
 
-import com.example.screencatalog.model.EpisodeData;
-import com.example.screencatalog.model.SeasonData;
-import com.example.screencatalog.model.SerieData;
-import com.example.screencatalog.service.ApiConsumption;
-import com.example.screencatalog.service.ConvertData;
+import com.example.screencatalog.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreencatalogApplication implements CommandLineRunner { //Interface to run application in command line
@@ -21,44 +14,7 @@ public class ScreencatalogApplication implements CommandLineRunner { //Interface
 
 	@Override
 	public void run(String... args) throws Exception { //Implements run method
-		/* Create API consumption object */
-		var apiConsumption = new ApiConsumption();
-
-		/* Make a request and return a string */
-		var json = apiConsumption.getData("http://www.omdbapi.com/?t=gilmore+girls&apikey=306f71f9");
-		System.out.println(json);
-
-		/* Create converter string to object */
-		ConvertData converter = new ConvertData();
-
-		/* Convert a string into a SerieData object */
-		SerieData newSerie = converter.getData(json, SerieData.class);
-		System.out.println(newSerie);
-
-		/* Make a new request and return a string */
-		json = apiConsumption.getData("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=306f71f9");
-
-		/* Convert a string into a EpisodeData object */
-		EpisodeData newEpisode = converter.getData(json, EpisodeData.class);
-		System.out.println(newEpisode);
-
-		/* Create an Arraylist of seasons */
-		List<SeasonData> seasons = new ArrayList<>();
-
-		/* Iterate through the seasons */
-		for (int i = 1; i <= newSerie.totalSeasons(); i++) {
-			/* Make a new request and return a string */
-			json = apiConsumption.getData("https://www.omdbapi.com/?t=gilmore+girls&season=" + i +
-					"&apikey=306f71f9");
-
-			/* Convert a string into a SeasonData object */
-			SeasonData newSeason = converter.getData(json, SeasonData.class);
-
-			/* Add to Arraylist */
-			seasons.add(newSeason);
-		}
-
-		/* Print the seasons */
-		seasons.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.showMenu(); //show menu
 	}
 }
--- src/main/java/com/example/screencatalog/principal/Principal.java
+++ src/main/java/com/example/screencatalog/principal/Principal.java
@@ -1,13 +1,19 @@
 package com.example.screencatalog.principal;
 
+import com.example.screencatalog.model.SeasonData;
+import com.example.screencatalog.model.SerieData;
 import com.example.screencatalog.service.ApiConsumption;
+import com.example.screencatalog.service.ConvertData;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /* Class that show the menu */
 public class Principal {
     private Scanner input = new Scanner(System.in);
     private ApiConsumption apiConsumption = new ApiConsumption(); //object to consume the API
+    private ConvertData converter = new ConvertData(); //object to convert string to object
     private final String ADDRESS = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=306f71f9";
 
@@ -18,5 +24,28 @@ public class Principal {
 
         /* Make a request and return a string */
         var json = apiConsumption.getData(ADDRESS + serieName + API_KEY);
+
+        /* Convert a string into a SerieData object */
+        SerieData newSerie = converter.getData(json, SerieData.class);
+
+        System.out.println(newSerie);
+
+        /* Create an Arraylist of seasons */
+		List<SeasonData> seasons = new ArrayList<>();
+
+		/* Iterate through the seasons */
+		for (int i = 1; i <= newSerie.totalSeasons(); i++) {
+			/* Make a new request with the season and return a string */
+			json = apiConsumption.getData(ADDRESS + serieName + "&season=" + i + API_KEY);
+
+			/* Convert a string into a SeasonData object */
+			SeasonData newSeason = converter.getData(json, SeasonData.class);
+
+			/* Add to Arraylist */
+			seasons.add(newSeason);
+		}
+
+		/* Print the seasons */
+		seasons.forEach(System.out::println);
     }
 }
