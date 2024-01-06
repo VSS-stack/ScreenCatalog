1d741028071ce5868ecb6705f5ddb12cf7a73f37
VSS-stack <victor.stsilva93@gmail.com>
2024-01-06 15:51:02 -0300

    Making a new request and showing the seasons of a tv serie

--- src/main/java/com/example/screencatalog/ScreencatalogApplication.java
+++ src/main/java/com/example/screencatalog/ScreencatalogApplication.java
@@ -1,12 +1,16 @@
 package com.example.screencatalog;
 
 import com.example.screencatalog.model.EpisodeData;
+import com.example.screencatalog.model.SeasonData;
 import com.example.screencatalog.model.SerieData;
 import com.example.screencatalog.service.ApiConsumption;
 import com.example.screencatalog.service.ConvertData;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreencatalogApplication implements CommandLineRunner { //Interface to run application in command line
@@ -37,5 +41,24 @@ public class ScreencatalogApplication implements CommandLineRunner { //Interface
 		/* Convert a string into a EpisodeData object */
 		EpisodeData newEpisode = converter.getData(json, EpisodeData.class);
 		System.out.println(newEpisode);
+
+		/* Create an Arraylist of seasons */
+		List<SeasonData> seasons = new ArrayList<>();
+
+		/* Iterate through the seasons */
+		for (int i = 1; i <= newSerie.totalSeasons(); i++) {
+			/* Make a new request and return a string */
+			json = apiConsumption.getData("https://www.omdbapi.com/?t=gilmore+girls&season=" + i +
+					"&apikey=306f71f9");
+
+			/* Convert a string into a SeasonData object */
+			SeasonData newSeason = converter.getData(json, SeasonData.class);
+
+			/* Add to Arraylist */
+			seasons.add(newSeason);
+		}
+
+		/* Print the seasons */
+		seasons.forEach(System.out::println);
 	}
 }
